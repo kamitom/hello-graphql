4ee4c4e5c385ade0d52e8bfb0cc05bc942ccf53e
Tom Osaka <tom825416@gmail.com>
2018-11-29 20:11:35 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const { makeExecutableSchema } = require('graphql-tools');
+const { graphql } = require('graphql');
+
+const typeDefs = `
+schema {
+    query: Query
+}
+type Query {
+    hello: String
+    hello123: String
+}
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'World',
+        hello123: () => 'test graphQL!!'
+    }
+};
+
+const schema = makeExecutableSchema({typeDefs, resolvers});
+
+const query = process.argv[2];
+
+graphql(schema, query).then(result => {
+  
+    console.log(JSON.stringify(result, null, 2));
+})
